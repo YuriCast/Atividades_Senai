8f65f3e4f4f0f366611c733dbe3de318d775fde3
Yuri Castilho <99267254+YuriCast@users.noreply.github.com>
2022-10-20 22:36:29 -0300

    Add files via upload

--- 04_10/script.js
+++ 04_10/script.js
@@ -1,51 +1,52 @@
 // 1° resposta
 
-let nota1 = parseFloat(prompt("digite nota 1"));
-let nota2 = parseFloat(prompt("Digite a nota 2"));
-media = (nota1 + nota2) / 2;
-console.log(media);
-if (media >= 6.0) {
-  console.log("aprovado");
-} else {
-  console.log("exame");
-  let exame = parseFloat(prompt("Digite a nota no exame"));
-  let final = (media + exame) / 2;
-  console.log(final);
-
-  if (final >= 6.0) {
-    console.log("Aprovado");
+let num = parseFloat(prompt("Digite um numero"));
+if (num >= 0 && num < 100) {
+  if (num <= 25) {
+    console.log("Intervalo [0,25]");
+  } else if (num <= 50) {
+    console.log("Intervalo (25, 50]");
+  } else if (num <= 75) {
+    console.log("Intervalo (50, 75]");
   } else {
-    console.log("Reprovado");
+    console.log("Intervalo (75, 100]");
+  }
+} else {
+  console.log("Fora do intervalo");
+}
+
+// 2° resposta
+
+let num1 = Number(prompt("Insira um número: "));
+let num2 = Number(prompt("Insira outro número: "));
+
+if (num1 > num2) {
+  if (num1 % num2 == 0) {
+    console.log("Sao multiplos!");
+  } else {
+    console.log("Nao sao multiplos!");
+  }
+} else {
+  if (num2 % num1 == 0) {
+    console.log("Sao multiplos!");
+  } else {
+    console.log("Nao sao multiplos!");
   }
 }
 
-//2ª Resposta
+// 3° resposta
 
-let num = parseInt(prompt("Digite um numero"));
+// 4° resposta
 
-if (num == 0) console.log("Número igual a zero");
-else if (num > 0) console.log("Número positivo");
-else console.log("Número Negativo");
+var horaInicio = parseFloat(prompt("Insira a hora de início do jogo"));
+var horaFim = parseFloat(prompt("Insira a hora de fim do jogo"));
 
-// 3° Resposta
+if (horaInicio >= horaFim) {
+  var duracao = 24 - horaInicio + horaFim;
+} else {
+  var duracao = horaFim - horaInicio;
+}
 
-let num1 = parseInt(prompt("Digite um número"));
-let num2 = parseInt(prompt("Digite outro número"));
+console.log("O jogo durou " + duracao + " horas");
 
-if (num1 > num2) console.log(num1);
-else console.log(num2);
-
-// 4° Resposta
-
-let sexo = String(prompt("Qual seu gênero? Ex: M ou F"));
-if (sexo == "F") console.log("Você é uma mulher");
-else if (sexo == "M") console.log("Você é um homem");
-else console.log("Sexo inválido");
-
-// 5° Resposta
-
-let idade = parseInt(prompt("Qual a sua idade?"));
-if (idade <= 10) console.log("Infantil");
-else if (idade >= 11 && idade <= 13) console.log("Infanto Juvenil");
-else if (idade >= 14 && idade <= 18) console.log("Juvenil");
-else console.log("Adulto");
+// 5° resposta
